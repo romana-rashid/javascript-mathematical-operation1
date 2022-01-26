c1e7efa97f35cf028dc82a4bc122aacf3cd29042
Romana Rashid <romanacse98@gmail.com>
2022-01-26 19:37:55 +0600

    new operation

---
+++ math-operation.js
@@ -0,0 +1,30 @@
+var price1 = 20;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price1 - price2;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+var modulous= price1 % price2;
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+// console.log(modulous);
+
+
+var doublePrice = price2 * 20;
+// var newPrice = price1 + 10;
+//price1 = price1 + 10;//price1 is already defined
+price1 += 10; 
+//price2 = price2 - 5;
+price2 -= 5;
+var age = 25;
+var love = 100;
+//age = age + 1;
+age++;
+//love = love - 1;
+love--;
+console.log(price1);
+console.log(price2);
+console.log(age);
+console.log(love);
