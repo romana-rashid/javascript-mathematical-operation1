9a98fd335361b7c84dfa36f87241087f005402f7
Romana Rashid <romanacse98@gmail.com>
2022-01-26 20:28:42 +0600

    javaScript conversion

---
+++ conversion.js
@@ -0,0 +1,26 @@
+var firtName = 'Romana';
+var lastName = 'Rashid';
+var fullName = firtName + ' ' + lastName;
+console.log(fullName);
+
+var onionPrice = '41';
+var eggPrice = '45';
+var total = onionPrice + eggPrice;
+console.log(total);
+
+var mangoPrice = '50';
+var guavaPrice = 80;
+var sum = mangoPrice + guavaPrice;
+console.log(sum);
+
+//iinteger
+var sunglass = 3;
+//float
+var price = 99.99;
+
+var onionPrice = '41.50';
+var eggPrice = '45.33';
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+console.log(onionPriceNumber);
+console.log(eggPriceNumber);
